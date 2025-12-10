3897b43a66cde87384870fe414ac0b3250c5591b
Colin Reilly <colinrlly@gmail.com>
2025-12-09 21:24:22 -0700

    Colors

--- src/components/ui/report-content.tsx
+++ src/components/ui/report-content.tsx
@@ -6,7 +6,7 @@ interface ReportContentProps {
 
 export function ReportContent({ selectedId }: ReportContentProps) {
   return (
-    <div className="flex-1 bg-[#c4b998] p-4 flex items-center justify-center">
+    <div className="flex-1 bg-[#c8b9a9] p-4 flex items-center justify-center">
       <div className="win98-border-sunken bg-[#a8a8a8] w-full h-full flex items-center justify-center text-[#666]">
         {selectedId}
       </div>
